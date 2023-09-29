f1ad517124f5912a68cdf53a210204cc488dd5f7
Frajtel <jkbstfnk@gmail.com>
2023-09-29 09:16:13 +0200

    Kalkulačka 1.hod
    Kalkulačka

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Vítajte v kalkulačke: ");
+Console.WriteLine("Zadajte prvé číslo: ");
+string s = Console.ReadLine();
+float a = int.Parse(s);
+Console.WriteLine("Zadajte druhé číslo");
+string z = Console.ReadLine();
+float b = int.Parse(z);
+
+float sučetF, rozdielF, podielF, súčinF;
+sučetF = a + b;
+súčinF = a * b;
+rozdielF = a - b;
+podielF = a / b;
+
+Console.WriteLine("Súčet: " + sučetF );
+Console.WriteLine("Rozdiel: " + rozdielF );
+Console.WriteLine("Podiel: " + podielF );
+Console.WriteLine("Súčin: " + súčinF );
+Console.WriteLine("Ďakujem za použitie kalulačky, aplikaciu ukončite ľubovolnou klávesou.: ");
